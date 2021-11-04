21d13d13c499b1d698467144bad356b9e193bfd4
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-11-04 09:51:08 +0100

    added redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './redux/store';
 import './index.css';
 import App from './App';
+
+console.log(store);
 
 ReactDOM.render(
   <React.StrictMode>
